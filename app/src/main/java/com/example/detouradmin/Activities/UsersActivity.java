8c067e9da2491f6bbfbe9cd960fc32e14214a7d3
Tona <tonadaphine77@gmail.com>
2021-04-21 17:47:35 +0200

    Edited user profile

--- app/src/main/java/com/example/detouradmin/Activities/UsersActivity.java
+++ app/src/main/java/com/example/detouradmin/Activities/UsersActivity.java
@@ -1,6 +1,7 @@
 package com.example.detouradmin.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,7 +20,14 @@ import com.example.detouradmin.Models.UserModel;
 import com.example.detouradmin.R;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 
 import butterknife.BindView;
@@ -34,6 +42,10 @@ public class UsersActivity extends AppCompatActivity {
 
     private FirebaseFirestore firestore;
     private FirestoreRecyclerAdapter userAdapter;
+    private FirebaseAuth mAuth;
+
+    Dialog myDialog;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +57,7 @@ public class UsersActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Users");
 
         firestore = FirebaseFirestore.getInstance();
+        myDialog = new Dialog(this);
 
         //Query
         Query query = firestore.collection("users");
@@ -102,6 +115,58 @@ public class UsersActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         userAdapter.startListening();
+    }
+
+    public void showPopUp(View v){
+
+        ImageView cancelDialog;
+        TextView userNameDialog;
+        TextView userEmailDialog;
+        TextView userMobileDialog;
+
+        myDialog.setContentView(R.layout.item_user);
+
+        cancelDialog =(ImageView) myDialog.findViewById(R.id.cancel);
+        userNameDialog =(TextView) myDialog.findViewById(R.id.userName);
+        userEmailDialog =(TextView) myDialog.findViewById(R.id.userEmail);
+        userMobileDialog =(TextView) myDialog.findViewById(R.id.userMobile);
+
+
+        for (UserInfo user: FirebaseAuth.getInstance().getCurrentUser().getProviderData()) {
+            if (user.getProviderId().equals("facebook.com") || user.getProviderId().equals("google.com")) {
+                final FirebaseUser mUser = mAuth.getCurrentUser();
+                String userName = mUser.getDisplayName();
+                String email = mUser.getEmail();
+                String phone = mUser.getPhoneNumber();
+
+                userNameDialog.setText(userName);
+                userEmailDialog.setText(email);
+                userMobileDialog.setText(phone);
+            }
+            else if (user.getProviderId().equals("password")) {
+                userMobileDialog.setVisibility(View.VISIBLE);
+
+                userID = mAuth.getCurrentUser().getUid();
+
+                DocumentReference documentReference = firestore.collection("users").document(userID);
+                documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                        userNameDialog.setText(documentSnapshot.getString("Username"));
+                        userEmailDialog.setText(documentSnapshot.getString("Email"));
+                        userMobileDialog.setText(documentSnapshot.getString("Phone"));
+                    }
+                });
+            }
+        }
+
+        cancelDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.show();
     }
 
 }
